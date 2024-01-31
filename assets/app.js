34b18db9dca42d128c6b4cc15ca0620bcb6ffd0c
Hristo Georgiev <h.georgiev@hotmail.com>
2024-01-31 20:47:57 +0200

    WIP - introduce htmx; Redo some templating

--- assets/app.js
+++ assets/app.js
@@ -1,9 +1,61 @@
-/*
- * Welcome to your app's main JavaScript file!
- *
- * We recommend including the built version of this JavaScript file
- * (and its CSS file) in your base layout (base.html.twig).
- */
-
-// any CSS you import will output into a single css file (app.css in this case)
 import './styles/app.css';
+import 'htmx.org';
+
+window.htmx = require('htmx.org');
+
+const MainMenu = () => {
+  const onLoad = (attributes) => {};
+  return { onLoad };
+};
+
+const PeriodRangeMenu = () => {
+  const id = 'periodRangeButtons';
+  const menuElem = window.document.getElementById(id);
+  const btns = {
+    day: 'todayRangeButton',
+    week: 'weekRangeButton',
+    month: 'monthRangeButton',
+    year: 'yearRangeButton',
+  };
+
+  const hide = () => {
+    menuElem.style.display = 'none';
+  };
+
+  const show = () => {
+    menuElem.style.display = 'block';
+  };
+
+  const onLoad = ({ page, period }) => {
+    ['songs', 'artists'].indexOf(page) < 0 ? hide() : show();
+
+    for (const p in btns) {
+      const btn = window.document.getElementById(btns[p]);
+      btn.classList.remove('active');
+      if (p == period) {
+        btn.classList.add('active');
+      }
+
+      btn.setAttribute('hx-get', `/charts/${page}/${p}`);
+      btn.setAttribute('hx-trigger', 'click');
+      btn.setAttribute('hx-target', '#main-pane');
+      htmx.process(menuElem);
+    }
+  };
+
+  return { onLoad };
+};
+
+const mainMenu = MainMenu();
+const periodRangeMenu = PeriodRangeMenu();
+
+htmx.on('htmx:load', (e) => {
+  const attrKeys = Array.from(e.detail.elt.attributes).map((x) => x.nodeName);
+  const attributes = {};
+  for (const k of attrKeys) {
+    const newKeyName = k.replace(/^data\-/, '');
+    attributes[newKeyName] = e.detail.elt.attributes.getNamedItem(k).nodeValue;
+  }
+  mainMenu.onLoad(attributes);
+  periodRangeMenu.onLoad(attributes);
+});
--- src/Controller/IndexController.php
+++ src/Controller/IndexController.php
@@ -3,14 +3,14 @@
 namespace App\Controller;
 
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
-use Symfony\Component\HttpFoundation\RedirectResponse;
+use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
 
 class IndexController extends AbstractController
 {
     #[Route('/', name: 'app_index')]
-    public function index(): RedirectResponse
+    public function index(): Response
     {
-        return $this->redirectToRoute('app_song_chart');
+        return $this->render('base.html.twig');
     }
 }
